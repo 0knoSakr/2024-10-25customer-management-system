8f6d8b40aa26f96e3813a5c5b89166b8befb3180
0knoSakr <sakura.okano@gmail.com>
2024-10-25 17:59:45 +0900

    backendのrouterの設定途中

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -1,1 +1,10 @@
 //顧客情報のルーティング
+
+//expressをインポートしてルーター機能を使用可能にする
+const express = require('express');
+//ユーザー関連のコントローラーをインポートする
+const userController = require('../controllers/customerController');
+//expressのルーターオブジェクトを作成するここでルーターが使えるようになる
+const router = express.Router;
+
+router.get('/customers', customerContoroller.allCustomer);
