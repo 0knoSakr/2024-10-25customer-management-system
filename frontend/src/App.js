bd2c4a2c2cfd8075a4c5c3f77b21cf623b2a4209
0knoSakr <sakura.okano@gmail.com>
2024-10-30 10:28:52 +0900

    axiosの設定ファイルを追加 API呼び出しの記述を追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,25 @@
 //ルーティングと全体構造
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Linkt to="/">顧客一覧</Linkt>
+            </li>
+            <li>
+              <Link>顧客詳細</Link>
+            </li>
+            <li>
+              <Link>顧客追加/編集</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,23 @@
-//API呼び出し用のサービス
+//API呼び出し
+
+import api from './productService';
+//顧客一覧取得
+export const allCustomer = (id) => {
+  return api.get(`/customers`);
+};
+//顧客詳細取得
+export const getCustomer = (id) => {
+  return api.get(`/customers/${id}`);
+};
+//顧客追加
+export const postCustomer = (id) => {
+  return api.post(`/customers`);
+};
+//顧客更新
+export const updateCustomer = (id) => {
+  return api.put(`/customers/${id}`);
+};
+//顧客削除
+export const deleteCustomer = (id) => {
+  return api.delete(`/customers/${id}`);
+};
