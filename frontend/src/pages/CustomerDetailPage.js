df94358d6924b9d2fbf9bf4d8335f0e5dd7cd837
0knoSakr <sakura.okano@gmail.com>
2024-11-05 15:52:42 +0900

    Datailに情報を取得して表示するための記述を追加

--- frontend/src/pages/CustomerDetailPage.js
+++ frontend/src/pages/CustomerDetailPage.js
@@ -1,5 +1,5 @@
 //顧客詳細画面
-import React { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../services/api';//データを取得するためのapi
 import { useParams } from 'react-router-dom';//idから情報をとれるようになる
 
@@ -10,7 +10,7 @@ const CustomerDetailPage = () => {
   useEffect(() => {
     const fetchCustomer = async () => {
       try {
-        const response = await api.get(`http://localsot:5000/api/customers/${id}`);
+        const response = await api.get(`/customers/${id}`);
         setCustomer(response.data);
       } catch (error) {
         console.error("商品情報の取得に失敗しました", error);
@@ -25,12 +25,8 @@ const CustomerDetailPage = () => {
 
   return (
     <div>
-      <h2>顧客詳細</h2>
-      <p>名前:{ customer[0].name }</p>
-      <p>email:{ customer[0].email }</p>
-      <p>電話番号:{ customer[0].phone }</p>
-      <p>住所:{ customer[0].address }</p>
-      <p>会社名:{ customer[0].company_name }</p>
+      <h1>顧客詳細</h1>
+      <CustomerDetail customer={ customers }/>
     </div>
   );
 };
