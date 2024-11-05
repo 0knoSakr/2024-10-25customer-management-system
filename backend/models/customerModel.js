73693fa157a82bbf6c08e97655b9ff7ec842bc17
0knoSakr <sakura.okano@gmail.com>
2024-11-05 18:57:34 +0900

    モジュールの追記

--- backend/models/customerModel.js
+++ backend/models/customerModel.js
@@ -1,41 +1,48 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const Customers = {
-    create: (data, callback) => {
-        const query = `INSERT INTO customers (name, email, phone, address) VALUES (?, ?, ?)`;
+  create: (data, callback) => {
+    const query = `INSERT INTO customers (name, email, phone, address) VALUES (?, ?, ?)`;
 
-        db.query(query, [data.name, data.email, data.phone, data.address], callback);
-    },
+    db.query(
+      query,
+      [data.name, data.email, data.phone, data.address],
+      callback
+    );
+  },
 
-    findByEmail: (email, callback) => {
-        const query = `SELECT * FROM customers WHERE email = ?`;
+  findByEmail: (email, callback) => {
+    const query = `SELECT * FROM customers WHERE email = ?`;
 
-        db.query(query, [email], (err, results) => {
-            if (err) return callback(err, null);
+    db.query(query, [email], (err, results) => {
+      if (err) return callback(err, null);
 
-            if (results.length > 0) return callback(null, results[0]);
+      if (results.length > 0) return callback(null, results[0]);
 
-            return callback(null, null);
-        });
-    },
+      return callback(null, null);
+    });
+  },
 
-    findAll: (callback) => {
-        const query = `SELECT * FROM customers`;
+  findAll: (callback) => {
+    const query = `SELECT * FROM customers`;
 
-        db.query(query, callback);
-    },
+    db.query(query, callback);
+  },
 
-    updateById: (id, data, callback) => {
-        const query = `UPDATE customers SET name = ?, email = ?, phone = ?, address = ?, WHERE id = ?`;
+  updateById: (id, data, callback) => {
+    const query = `UPDATE customers SET name = ?, email = ?, phone = ?, address = ? WHERE id = ?`; // 不必要なコンマを削除
 
-        db.query(query, [data.name, data.email, data.phone, data.address, id], callback);
-    },
+    db.query(
+      query,
+      [data.name, data.email, data.phone, data.address, id],
+      callback
+    );
+  },
+  deleteById: (id, callback) => {
+    const query = `DELETE FROM customers WHERE id = ?`;
 
-    deleteById: (id, callback) => {
-        const query = `DELETE FROM customers WHERE id = ?`;
-
-        db.query(query, [id], callback);
-    }
+    db.query(query, [id], callback);
+  },
 };
 
 module.exports = Customers;
