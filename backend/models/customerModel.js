46ea25f667bcb025f2c37449ae7de78143aefe12
0knoSakr <sakura.okano@gmail.com>
2024-11-06 15:41:56 +0900

    データベースアクセス用のロジックを追記コントローラーのgetCustomerとcustomerRoutesにrouteを設定

--- backend/models/customerModel.js
+++ backend/models/customerModel.js
@@ -3,7 +3,6 @@ const db = require("../config/db");
 const Customers = {
   create: (data, callback) => {
     const query = `INSERT INTO customers (name, email, phone, address) VALUES (?, ?, ?)`;
-
     db.query(
       query,
       [data.name, data.email, data.phone, data.address],
@@ -11,38 +10,30 @@ const Customers = {
     );
   },
 
-  findByEmail: (email, callback) => {
-    const query = `SELECT * FROM customers WHERE email = ?`;
-
-    db.query(query, [email], (err, results) => {
-      if (err) return callback(err, null);
-
-      if (results.length > 0) return callback(null, results[0]);
-
-      return callback(null, null);
-    });
-  },
-
   findAll: (callback) => {
     const query = `SELECT * FROM customers`;
-
     db.query(query, callback);
   },
 
   updateById: (id, data, callback) => {
-    const query = `UPDATE customers SET name = ?, email = ?, phone = ?, address = ? WHERE id = ?`; // 不必要なコンマを削除
-
+    const query = `UPDATE customers SET name = ?, email = ?, phone = ?, address = ? WHERE id = ?`;
     db.query(
       query,
       [data.name, data.email, data.phone, data.address, id],
       callback
     );
   },
-  deleteById: (id, callback) => {
-    const query = `DELETE FROM customers WHERE id = ?`;
 
-    db.query(query, [id], callback);
-  },
-};
-
+  findById: (id) => {
+    const query = `SELECT * FROM customers WHERE id = ?`;
+    return new Promise((resolve, reject) => {
+      db.query(query, [id], (error, results) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(results);
+      });
+    });
+  }
+  }
 module.exports = Customers;
